53e3c000c94f3c556657134c8123d05cbda8060b
Laura Ribes <lauraribes@BCN001DEV046.local>
2022-03-11 11:58:26 +0100

    Register micro home app

--- src/aizon-bigfinite-root-config.ts
+++ src/aizon-bigfinite-root-config.ts
@@ -13,18 +13,13 @@ registerApplication({
 
 registerApplication({
   name: "@aizon-bigfinite/micro-home",
-  app: () =>
-    System.import(
-      "@aizon-bigfinite/micro-home"
-    ),
-    activeWhen: [(location) => location.pathname === "/"],
-    customProps: {
-      authToken: "123456789x0",
-      agileEstimation: Estimation.StoryPoints,
-    },
+  app: () => System.import("@aizon-bigfinite/micro-home"),
+  activeWhen: [(location) => location.pathname === "/"],
+  customProps: {
+    authToken: "123456789x0",
+    agileEstimation: Estimation.StoryPoints,
+  },
 });
-
-
 
 registerApplication({
   name: "@aizon-bigfinite/micro-pointing-poker",
